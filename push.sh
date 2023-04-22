c9d562fccfd5ba9b611a92029494ea5e715b47f5
Leo <leohuangyi@foxmail.com>
2023-04-22 17:41:34 +0800

    Improvement in functionality with addition of colored output for notifications.

--- push.sh
+++ push.sh
@@ -1,67 +1,101 @@
 #!/bin/bash
 
-REMOTE=origin
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-LOCAL_COMMITS=$(git rev-list --count HEAD ^$REMOTE/$BRANCH)
-BATCH_SIZE=$((LOCAL_COMMITS / 10 + 1))
+coloredEcho () {
+    local color=$1
+    shift
+    printf "$(tput bold)$(tput setaf $color)%s$(tput sgr0)\n" "$@"
+}
 
-# check if the branch exists on the remote
-if git show-ref --quiet --verify refs/remotes/$REMOTE/$BRANCH; then
-    # if so, only push the commits that are not on the remote already
-    range=$REMOTE/$BRANCH..HEAD
-    REMOTE_COMMITS=$(git rev-list --count $REMOTE/$BRANCH..HEAD)
-else
-    # else push all the commits
-    range=HEAD
-    REMOTE_COMMITS=0
-fi
-# count the number of commits to push
-n=$(git log --first-parent --format=format:x $range | wc -l)
+pushCommits () {
+    local remote=$1
+    local branch=$2
+    local range=$3
+    local batch_size=$4
+    local local_commits=$5
+    local remote_commits=$6
+    local counter=1
+    local retries=3
 
-# initialize counter
-counter=1
+    # push each batch
+    for i in $(seq $local_commits -$batch_size 1); do
+        # get the hash of the commit to push
+        local h=$(git log --first-parent --reverse --format=format:%H --skip $i -n1)
+        coloredEcho 6 "Pushing commit $counter/$local_commits (remote synced $remote_commits): $h..."
 
-# push each batch
-for i in $(seq $n -$BATCH_SIZE 1); do
-    # get the hash of the commit to push
-    h=$(git log --first-parent --reverse --format=format:%H --skip $i -n1)
-    echo "Pushing commit $counter/$LOCAL_COMMITS (remote synced $REMOTE_COMMITS): $h..."
-    # push the commit and check the return value
+        # push the commit and check the return value
+        success=false
+        while ! $success && [ $retries -gt 0 ]; do
+            if git push --force-with-lease $remote $h:refs/heads/$branch; then
+                success=true
+            else
+                coloredEcho 1 "Pushing failed. Retrying..."
+                retries=$((retries-1))
+                sleep 1
+            fi
+        done
+
+        # check if the retry count has been exhausted
+        if ! $success; then
+            coloredEcho 1 "Pushing failed. Aborting."
+            exit 1
+        fi
+
+        # increment counters
+        counter=$((counter+1))
+        remote_commits=$((remote_commits+1))
+        retries=3
+    done
+
+    # push the final partial batch
     success=false
-    retries=0
-    until $success || [ $retries -eq 3 ]; do
-        if git push $REMOTE $h:refs/heads/$BRANCH; then
+    while ! $success && [ $retries -gt 0 ]; do
+        if git push --force-with-lease $remote HEAD:refs/heads/$branch; then
             success=true
         else
-            echo "Pushing failed. Retrying..."
-            retries=$((retries+1))
+            coloredEcho 1 "Pushing failed. Retrying..."
+            retries=$((retries-1))
             sleep 1
         fi
     done
+
     # check if the retry count has been exhausted
     if ! $success; then
-        echo "Pushing failed. Retry limit exceeded. Aborting."
+        coloredEcho 1 "Pushing failed. Aborting."
         exit 1
     fi
-    # increment counters
-    counter=$((counter+1))
-    REMOTE_COMMITS=$((REMOTE_COMMITS+1))
-done
-# push the final partial batch
-success=false
-retries=0
-until $success || [ $retries -eq 3 ]; do
-    if git push $REMOTE HEAD:refs/heads/$BRANCH; then
-        success=true
-    else
-        echo "Pushing failed. Retrying..."
-        retries=$((retries+1))
-        sleep 1
-    fi
-done
-# check if the retry count has been exhausted
-if ! $success; then
-    echo "Pushing failed. Retry limit exceeded. Aborting."
+
+    coloredEcho 2 "Pushing completed successfully."
+}
+
+# Check if git is installed
+if ! command -v git &> /dev/null; then
+    coloredEcho 1 "Git is not installed. Aborting."
     exit 1
 fi
-echo "Pushing completed successfully."
+
+REMOTE=origin
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if the branch exists on the remote
+if git show-ref --quiet --verify refs/remotes/$REMOTE/$BRANCH; then
+    # If so, only push the commits that are not on the remote already
+    RANGE=$REMOTE/$BRANCH..HEAD
+    REMOTE_COMMITS=$(git rev-list --count $REMOTE/$BRANCH..HEAD)
+else
+    # Else push all the commits
+    RANGE=HEAD
+    REMOTE_COMMITS=0
+fi
+
+LOCAL_COMMITS=$(git rev-list --count HEAD ^$REMOTE/$BRANCH)
+
+# Count the number of commits to push
+N=$(git log --first-parent --format=format:x $RANGE | wc -l)
+
+# Calculate the batch size
+BATCH_SIZE=$((LOCAL_COMMITS / 10 + 1))
+
+coloredEcho 2 "REMOTE:$RANGE BATCH_SIZE:$BATCH_SIZE"
+
+# push the commits
+pushCommits $REMOTE $BRANCH $RANGE $BATCH_SIZE $LOCAL_COMMITS $REMOTE_COMMITS
